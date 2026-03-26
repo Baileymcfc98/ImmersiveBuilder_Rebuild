e065ac20fe5cc7f234809f06dbcdcb5d657ced6a
Baileymcfc98 <georgemcfc6@gmail.com>
2026-03-26 16:49:20 +0000

    Update app.js

--- js/app.js/app.js
+++ js/app.js/app.js
@@ -2,16 +2,16 @@
    APP ENTRY POINT – MODULAR REBUILD
    ============================================================ */
 
-import { PointerEngine } from "./js/pointer/pointerEngine.js";
-import { DragEngine } from "./js/pointer/dragEngine.js";
-import { ResizeEngine } from "./js/pointer/resizeEngine.js";
-import { CanvasEngine } from "./js/drawing/canvasEngine.js";
+import { PointerEngine } from "./pointer/pointerEngine.js";
+import { DragEngine } from "./pointer/dragEngine.js";
+import { ResizeEngine } from "./pointer/resizeEngine.js";
+import { CanvasEngine } from "./drawing/canvasEngine.js";
 
-import { PanelManager } from "./js/ui/panelManager.js";
-import { Toolbar } from "./js/ui/toolbar.js";
+import { PanelManager } from "./ui/panelManager.js";
+import { Toolbar } from "./ui/toolbar.js";
 
-import { ItemFactory } from "./js/items/itemFactory.js";
-``
+import { ItemFactory } from "./items/itemFactory.js";
+
 window.addEventListener("DOMContentLoaded", () => {
     console.log("App starting…");
 
